3b579004d4ceacf8a592adf1c0adf2e91a19998a
Gino Atlas <thraax.session@gino-atlas.de>
2020-12-04 22:28:53 +0100

    More playing

---
+++ src/main/kotlin/technology/iatlas/spaceup/controller/web/HomeController.kt
@@ -0,0 +1,29 @@
+package technology.iatlas.spaceup.controller.web
+
+import com.fizzed.rocker.Rocker
+import io.micronaut.http.MediaType
+import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Get
+import io.micronaut.security.annotation.Secured
+import org.slf4j.LoggerFactory
+
+
+@Controller("/")
+class HomeController {
+
+    @Get(produces = [MediaType.TEXT_HTML])
+    @Secured("isAnonymous()")
+    fun getHome(): String {
+
+        // Funktioniert irgendwie nicht
+        /*return base.template("World")
+            .render()
+            .toString();*/
+
+        return Rocker.template("views/home.rocker.html")
+            .bind("message", "Gino")
+            .render()
+            .toString()
+    }
+
+}
