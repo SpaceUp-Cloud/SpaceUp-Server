9dd1c27740ea9693c3ef134749095d513f21c718
Gino Atlas <thraax.session@gino-atlas.de>
2020-12-09 15:22:37 +0100

    SSL/HTTP2 + basic playing

--- src/main/kotlin/technology/iatlas/spaceup/controller/api/ExampleSSEController.kt
+++ src/main/kotlin/technology/iatlas/spaceup/controller/api/ExampleSSEController.kt
@@ -1,21 +1,16 @@
 package technology.iatlas.spaceup.controller.api
 
 import io.micronaut.http.MediaType
-import io.micronaut.http.annotation.Body
-import io.micronaut.http.annotation.Controller
-import io.micronaut.http.annotation.Get
-import io.micronaut.http.annotation.Post
+import io.micronaut.http.annotation.*
 import io.micronaut.http.sse.Event
 import org.reactivestreams.Publisher
 import org.slf4j.LoggerFactory
 import technology.iatlas.spaceup.services.SseServiceImpl
 
-@Controller("/time")
+@Controller("/sse")
 class ExampleSSEController {
     private val log = LoggerFactory.getLogger(ExampleSSEController::class.java)
-
-    private var sseService: SseServiceImpl<String>
-        = SseServiceImpl("update")
+    private var sseService: SseServiceImpl<String> = SseServiceImpl("update")
 
     @Get("/events", produces = [MediaType.TEXT_EVENT_STREAM])
     fun events(): Publisher<Event<String>> {
