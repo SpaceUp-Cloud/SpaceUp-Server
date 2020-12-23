a95f7fb28f1d6df1c69a1884e452a813e1f23473
Gino Atlas <thraax.session@gino-atlas.de>
2020-12-23 14:23:28 +0100

    It's now possible to create multiple themes for dark/light mode based on materializecss. Those files are scss files for easier handling.
    close: #6

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,11 +1,49 @@
 import ReconnectingEventSource from "./modules/ReconnectingSSE.js";
 
+$(document).ready(function() {
+    window.matchMedia('(prefers-color-scheme: dark)')
+        .addEventListener('change', event => {
+            if (event.matches) {
+                //dark mode
+                loadTheme("dark");
+            } else {
+                //light mode
+                loadTheme("light");
+            }
+        })
+})
+
 function initMaterialize() {
     M.AutoInit(undefined);
 }
 
+function loadTheme(mode) {
+
+    console.debug("Use " + mode + " theme");
+    $.ajax({
+        url: "/style/" + mode + ".css",
+        dataType: 'text',
+        success: function(data) {
+            // Remove old style
+            $("head").children().each(function(index, ele) {
+                if (ele.innerHTML && ele.innerHTML.substring(0, 30).match(/\/\*current theme\*\//)) {
+                    $(ele).remove();
+                    return false;    // Stop iterating since we removed something
+                }
+            });
+            $('<style type="text/css">/*current theme*/ \n' + data + '</style>').appendTo("head");
+        }
+    });
+}
+
 function init() {
     initMaterialize();
+
+    if(window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        loadTheme("dark");
+    } else {
+        loadTheme("light");
+    }
 
     let esSupport = (window.EventSource !== undefined);
     let result = document.getElementById("result");
@@ -40,4 +78,3 @@ function init() {
 }
 
 window.onload = init;
-
