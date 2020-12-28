8cc155b271272ea2b1af6dc78105bb3172382666
Gino Atlas <thraax.session@gino-atlas.de>
2020-12-28 15:28:30 +0100

    Added public/privatekey auth
    close: #24

--- src/main/kotlin/technology/iatlas/spaceup/services/SshService.kt
+++ src/main/kotlin/technology/iatlas/spaceup/services/SshService.kt
@@ -11,6 +11,7 @@ import technology.iatlas.spaceup.config.SpaceUpSshConfig
 import technology.iatlas.spaceup.core.cmd.CommandInf
 import java.io.BufferedReader
 import java.io.ByteArrayOutputStream
+import java.io.File
 import javax.inject.Inject
 import javax.inject.Singleton
 import kotlin.properties.Delegates
@@ -19,30 +20,41 @@ import kotlin.properties.Delegates
 class SshService(private val sshConfig: SpaceUpSshConfig) {
     private val log = LoggerFactory.getLogger(SshService::class.java)
 
+    private var session: Session
+
+    init {
+        val jsch = JSch()
+
+        val privatekey: String? = sshConfig.privatekey
+        if (privatekey != null && privatekey.isNotEmpty()) {
+            jsch.addIdentity(File(privatekey).normalize().path)
+            session = jsch.getSession(
+                sshConfig.username, sshConfig.host, Integer.valueOf(sshConfig.port!!))
+
+            log.info("Authenticate SSH via private key!")
+        } else {
+            session = jsch.getSession(
+                sshConfig.username, sshConfig.host, Integer.valueOf(sshConfig.port!!))
+            session.setPassword(sshConfig.password)
+
+            log.info("Authenticate SSH via password!")
+        }
+        session.setConfig("StrictHostKeyChecking", "no")
+        session.connect()
+    }
+
     fun execute(command: CommandInf): String {
         log.debug("Execute $command")
-
-        lateinit var session: Session
-        lateinit var channel: ChannelExec
+        val channel: ChannelExec = session.openChannel("exec") as ChannelExec
 
         try {
-            var port = 22
-            if(sshConfig.port != null) {
-                port = Integer.valueOf(sshConfig.port!!)
-            }
-            session = JSch().getSession(sshConfig.username,
-                sshConfig.host, port)
-            session.setPassword(sshConfig.password)
-            session.setConfig("StrictHostKeyChecking", "no")
-            session.connect()
-
-            channel = session.openChannel("exec") as ChannelExec
             channel.setCommand(command.parameters.joinToString(" "))
             val responseStream = ByteArrayOutputStream()
             channel.outputStream = responseStream
             channel.connect(10000)
 
-            while(channel.isConnected) {
+            // When then channel close itself, we retrieved the data
+            while (channel.isConnected) {
                 GlobalScope.run {
                     Thread.sleep(100)
                 }
@@ -53,7 +65,7 @@ class SshService(private val sshConfig: SpaceUpSshConfig) {
 
             return response
         } finally {
-            session.disconnect()
+            //session.disconnect()
             channel.disconnect()
         }
     }
