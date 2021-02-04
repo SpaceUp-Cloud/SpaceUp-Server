f6f004080bab43ae5e0254b319275143e8f34d10
Gino Atlas <thraax.session@gino-atlas.de>
2021-02-04 19:05:12 +0100

    Updated Websocket handling + too many small improvements to call

--- src/main/kotlin/technology/iatlas/spaceup/services/DomainService.kt
+++ src/main/kotlin/technology/iatlas/spaceup/services/DomainService.kt
@@ -1,6 +1,7 @@
 package technology.iatlas.spaceup.services
 
 import io.micronaut.context.env.Environment
+import io.micronaut.context.event.ApplicationEventPublisher
 import org.slf4j.LoggerFactory
 import technology.iatlas.spaceup.dto.Command
 import technology.iatlas.spaceup.core.cmd.Runner
@@ -9,20 +10,22 @@ import technology.iatlas.spaceup.core.parser.DeleteDomainParser
 import technology.iatlas.spaceup.core.parser.DomainParser
 import technology.iatlas.spaceup.dto.Domain
 import technology.iatlas.spaceup.dto.Feedback
+import technology.iatlas.spaceup.events.WebsocketResponseEvent
 import javax.inject.Singleton
 
 @Singleton
 class DomainService(
     private val env: Environment,
     private val sshService: SshService,
-    private val sseService: SseService<Feedback>
+    private val sseService: SseService<Feedback>,
+    private val eventPublisher: ApplicationEventPublisher
 ) {
     private val log = LoggerFactory.getLogger(DomainService::class.java)
 
     private val domainListRunner = Runner<List<Domain>>(env, sshService)
     private var domains = listOf<Domain>()
 
-    private val addDomainRunner =  Runner<Feedback>(env, sshService)
+    private val addDomainRunner = Runner<Feedback>(env, sshService)
 
     init {
         // Cache domain list for faster access but delay on fresh data
@@ -39,41 +42,55 @@ class DomainService(
     suspend fun add(domains: List<Domain>): List<Feedback> {
         sseService.eventName = "domain add"
         val cmd: MutableList<String> = mutableListOf("uberspace", "web", "domain", "add")
-        val feedbacks = mutableListOf<Feedback>()
+        val feedbacks = linkedSetOf<Feedback>()
 
         domains.forEach { domain ->
             // Add domain to cmd
             cmd.add(domain.url)
             addDomainRunner.execute(Command(cmd), CreateDomainParser())
 
-            addDomainRunner.getBehaviourSubject().subscribe {
-                when {
-                    it?.info!!.isNotEmpty() -> {
-                        val infoResponse = Feedback(info = "${domain.url}: ${it.info}", "")
-                        feedbacks.add(infoResponse)
+            addDomainRunner.getBehaviourSubject()
+                .doOnError { t ->
+                    val error = t.message
+                    val errorFeedback = error?.let { Feedback("", it) }
 
-                        sseService.publish(infoResponse)
-                    }
-                    it.error.isNotEmpty() -> {
-                        val errorResponse = Feedback(info = "", error = "${domain.url}: ${it.error}")
-                        feedbacks.add(errorResponse)
-
-                        sseService.publish(errorResponse)
-                    }
-                    // Should never happened actually
-                    else -> {
-                        feedbacks.add(it)
-                        sseService.publish(it)
+                    eventPublisher.publishEvent(WebsocketResponseEvent(errorFeedback))
+                    if (errorFeedback != null) {
+                        sseService.publish(errorFeedback)
                     }
                 }
+                .subscribe {
+                    when {
+                        it?.info!!.isNotEmpty() -> {
+                            val infoResponse = Feedback(info = "${domain.url}: ${it.info}", "")
+                            feedbacks.add(infoResponse)
 
-                // Remove domain from cmd
-                cmd.remove(domain.url)
-            }
+                            eventPublisher.publishEvent(WebsocketResponseEvent(infoResponse))
+                            sseService.publish(infoResponse)
+                        }
+                        it.error.isNotEmpty() -> {
+                            val errorResponse = Feedback(info = "", error = "${domain.url}: ${it.error}")
+                            feedbacks.add(errorResponse)
+
+                            eventPublisher.publishEvent(WebsocketResponseEvent(errorResponse))
+                            sseService.publish(errorResponse)
+                        }
+                        // Should never happened actually
+                        else -> {
+                            feedbacks.add(it)
+
+                            eventPublisher.publishEvent(WebsocketResponseEvent(it))
+                            sseService.publish(it)
+                        }
+                    }
+
+                    // Remove domain from cmd
+                    cmd.remove(domain.url)
+                }
         }
 
         log.debug("Response: $feedbacks")
-        return feedbacks
+        return feedbacks.toMutableList()
     }
 
     suspend fun delete(domain: Domain): Feedback {
@@ -85,10 +102,19 @@ class DomainService(
         runner.execute(Command(cmd), DeleteDomainParser())
         var feedback = Feedback("", "")
 
-        runner.getBehaviourSubject().subscribe {
-            sseService.publish(it)
-            feedback = it
-        }
+        runner.getBehaviourSubject()
+            .doOnError { t ->
+                // Should not happened
+                log.error(t.message)
+                feedback = t.message?.let { it -> Feedback("", it) }!!
+
+                eventPublisher.publishEvent(WebsocketResponseEvent(feedback))
+                sseService.publish(feedback)
+            }.subscribe {
+                eventPublisher.publishEvent(WebsocketResponseEvent(it))
+                sseService.publish(it)
+                feedback = it
+            }
 
         return feedback
     }
---
+++ src/main/kotlin/technology/iatlas/spaceup/websocket/WebsocketServer.kt
@@ -0,0 +1,53 @@
+package technology.iatlas.spaceup.websocket
+
+import io.micronaut.context.event.ApplicationEventListener
+import io.micronaut.http.annotation.PathVariable
+import io.micronaut.websocket.CloseReason
+import io.micronaut.websocket.WebSocketSession
+import io.micronaut.websocket.annotation.*
+import org.slf4j.LoggerFactory
+import technology.iatlas.spaceup.events.WebsocketResponseEvent
+
+@ServerWebSocket("/ws/{topic}")
+class WebsocketServer: ApplicationEventListener<WebsocketResponseEvent>{
+    private val log = LoggerFactory.getLogger(WebsocketServer::class.java)
+    private val sessions = hashMapOf<String, WebSocketSession>()
+
+    @OnOpen
+    fun onOpen(session: WebSocketSession, @PathVariable topic: String) {
+        log.info("Websocket open for ${session.id} on topic $topic")
+        sessions[session.id] = session
+    }
+
+    @OnMessage
+    fun onMessage(message: String, session: WebSocketSession, @PathVariable topic: String) {
+        log.debug("Websocket received message $message for topic $topic")
+    }
+
+    @OnClose
+    fun onClose(closeReason: CloseReason, session: WebSocketSession, @PathVariable topic: String) {
+        log.warn("Websocket for $topic closed because ${closeReason.reason}")
+        session.remove(session.id)
+    }
+
+    @OnError
+    fun onError(t: Throwable, @PathVariable topic: String) {
+        log.error("Websocket on $topic an error happened because ${t.message}")
+    }
+
+    /**
+     * Will be used to send messages to the client
+     */
+    private fun publish(message: WebsocketResponseEvent?, session: WebSocketSession) {
+        if(session.isOpen) {
+            session.sendAsync(message)
+        }
+    }
+
+    override fun onApplicationEvent(event: WebsocketResponseEvent?) {
+        log.debug("Broadcast message: $event")
+        sessions.forEach { (_, session) ->
+            sessions[session.id]?.let { publish(event, it) }
+        }
+    }
+}
