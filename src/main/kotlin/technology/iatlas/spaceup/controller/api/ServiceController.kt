42872428538078d9874c750cad2cdb720a235997
Thraax Session <thraax.session@gino-atlas.de>
2021-02-21 11:34:19 +0100

    Added API for service list + fixed service of Service
    Signed-off-by: Thraax Session <thraax.session@gino-atlas.de>

--- src/main/kotlin/technology/iatlas/spaceup/controller/api/ServiceController.kt
+++ src/main/kotlin/technology/iatlas/spaceup/controller/api/ServiceController.kt
@@ -1,6 +1,8 @@
 package technology.iatlas.spaceup.controller.api
 
+import io.micronaut.http.MediaType
 import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Get
 import io.micronaut.http.annotation.Post
 import org.slf4j.LoggerFactory
 import technology.iatlas.spaceup.dto.Feedback
@@ -12,13 +14,22 @@ import technology.iatlas.spaceup.services.ServiceService
 class ServiceController(private val serviceService: ServiceService) {
     private val log = LoggerFactory.getLogger(ServiceController::class.java)
 
+    /**
+     * Execute an operation change to a Service
+     */
     @Post("/execute/{name}/{option}")
-    suspend fun execute(name: String, option: String): Feedback? {
+    suspend fun execute(name: String, option: ServiceOption): Feedback {
         log.debug("Execute $name: $option")
 
         val serviceName = Service(name, null, null)
-        val serviceOption = ServiceOption.valueOf(option)
+        return serviceService.execute(serviceName, option)
+    }
 
-        return serviceService.execute(serviceName, serviceOption)
+    /**
+     * Get all services with their statuses
+     */
+    @Get("/list", produces = [MediaType.APPLICATION_JSON])
+    suspend fun list(): List<Service> {
+        return serviceService.list()
     }
 }
--- src/main/kotlin/technology/iatlas/spaceup/services/ServiceService.kt
+++ src/main/kotlin/technology/iatlas/spaceup/services/ServiceService.kt
@@ -2,10 +2,10 @@ package technology.iatlas.spaceup.services
 
 import io.micronaut.context.env.Environment
 import io.micronaut.context.event.ApplicationEventPublisher
-import technology.iatlas.spaceup.dto.Command
 import technology.iatlas.spaceup.core.cmd.Runner
 import technology.iatlas.spaceup.core.parser.EchoParser
 import technology.iatlas.spaceup.core.parser.ServiceParser
+import technology.iatlas.spaceup.dto.Command
 import technology.iatlas.spaceup.dto.Feedback
 import technology.iatlas.spaceup.dto.Service
 import technology.iatlas.spaceup.dto.ServiceOption
@@ -20,45 +20,18 @@ class ServiceService(
     private val eventPublisher: ApplicationEventPublisher
     ) {
 
-    /**
-     * Get all service execution options
-     */
-    fun options(): List<ServiceOption> = ServiceOption.asList()
+    private val services: MutableList<Service> = mutableListOf()
+    private var serviceExecutionFeedback: Feedback = Feedback("", "")
 
-    /**
-     * Returns a list of all existing services with their statuses
-     */
-    suspend fun list(): List<Service>? {
-        val cmd: MutableList<String> = mutableListOf("supervisorctl", "status")
-        val runner = Runner<List<Service>>(env, sshService)
-        runner.execute(Command(cmd), ServiceParser())
-
-        var services = emptyList<Service>()
-        runner.subject().subscribe {
-            services = it
+    private val serviceListRunner = Runner<List<Service>>(env, sshService)
+    private val serviceExecRunner = Runner<String>(env, sshService)
+    init {
+        serviceListRunner.subject().subscribe {
+            services.clear()
+            services.addAll(it)
         }
 
-        return services
-    }
-
-    /**
-     * Execute the service with a service option @see {ServiceOptions}
-     *
-     * @return Feedback
-     * @see {Feedback}
-     */
-    suspend fun execute(serivce: Service, option: ServiceOption): Feedback {
-        sseService.eventName="service exec"
-
-        val cmd: MutableList<String> = mutableListOf("supervisorctl",
-            option.name.toLowerCase(), serivce.name)
-
-        val runner = Runner<String>(env, sshService)
-        runner.execute(Command(cmd), EchoParser())
-
-        var resultFeedback = Feedback("", "")
-
-        runner.subject().subscribe {
+        serviceExecRunner.subject().subscribe {
             val feedback = if(it?.toLowerCase()!!.contains("(started|stopped)".toRegex())) {
                 Feedback(it, "")
             } else {
@@ -67,9 +40,38 @@ class ServiceService(
 
             eventPublisher.publishEvent(WebsocketFeedbackResponseEvent(feedback))
             sseService.publish(feedback)
-            resultFeedback = feedback
+            serviceExecutionFeedback = feedback
         }
+    }
 
-        return resultFeedback
+    /**
+     * Get all service execution options
+     */
+    fun options(): List<ServiceOption> = ServiceOption.asList()
+
+    /**
+     * Returns a list of all existing services with their statuses
+     */
+    suspend fun list(): List<Service> {
+        val cmd: MutableList<String> = mutableListOf("supervisorctl", "status")
+        serviceListRunner.execute(Command(cmd), ServiceParser())
+
+        return services
+    }
+
+    /**
+     * Execute the service with a service option {@link ServiceOption}
+     *
+     * @return Feedback
+     * @see Feedback
+     */
+    suspend fun execute(serivce: Service, option: ServiceOption): Feedback {
+        sseService.eventName="service exec"
+
+        val cmd: MutableList<String> = mutableListOf("supervisorctl",
+            option.name.toLowerCase(), serivce.name)
+
+        serviceExecRunner.execute(Command(cmd), EchoParser())
+        return serviceExecutionFeedback
     }
 }
