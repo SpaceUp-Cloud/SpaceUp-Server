9e35e7e0a00404f00c35faa6a169f9e274ff513f
Thraax Session <thraax.session@gino-atlas.de>
2021-04-17 15:45:24 +0200

    Improved websocket handling
    Signed-off-by: Thraax Session <thraax.session@gino-atlas.de>

--- src/main/kotlin/technology/iatlas/spaceup/services/WsBroadcaster.kt
+++ src/main/kotlin/technology/iatlas/spaceup/services/WsBroadcaster.kt
@@ -16,6 +16,16 @@ class WsBroadcaster(private val broadcaster: WebSocketBroadcaster) {
     }
 
     /**
+     * Broadcast synced a websocket message object with a specific topic filter
+     * @see WsBroadcaster.broadcastSync
+     */
+    public fun <T> broadcast(message: T, filterTopic: String) {
+        broadcaster.broadcastSync(message) {
+            it.attributes.asMap()["topic"]?.equals(filterTopic) ?: false
+        }
+    }
+
+    /**
      * Broadcast async a websocket message object with a specific topic filter
      */
     public fun <T> broadcastAsync(message: T, filterTopic: String) {
