ad5f9c45895faf985170d68119a4c83af20e676f
Thraax Session <thraax.session@iatlas.technology>
2021-08-02 16:41:34 +0200

    SU-14 more secured routes

--- src/main/kotlin/technology/iatlas/spaceup/controller/web/ServiceController.kt
+++ src/main/kotlin/technology/iatlas/spaceup/controller/web/ServiceController.kt
@@ -4,14 +4,18 @@ import com.fizzed.rocker.runtime.StringBuilderOutput
 import io.micronaut.http.MediaType
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
+import io.micronaut.security.annotation.Secured
+import io.micronaut.security.rules.SecurityRule
 import org.slf4j.LoggerFactory
 import technology.iatlas.spaceup.core.annotations.WebNavigation
 import technology.iatlas.spaceup.services.ServiceService
 
+@Secured(SecurityRule.IS_AUTHENTICATED)
 @Controller("/services")
 class ServiceController(private val serviceService: ServiceService) : BaseController() {
     private val log = LoggerFactory.getLogger(ServiceController::class.java)
 
+    @Secured(SecurityRule.IS_AUTHENTICATED)
     @WebNavigation("Services", "/services", prio = 1)
     @Get(produces = [MediaType.TEXT_HTML])
     suspend fun list(): String {
