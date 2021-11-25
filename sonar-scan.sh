934b5eaf38aac72f470459aaa7bdb28520ef9a50
Thraax Session <thraax.session@iatlas.technology>
2021-11-25 18:00:01 +0100

    added sonarqube

---
+++ sonar-scan.sh
@@ -0,0 +1,3 @@
+***REMOVED***!/usr/bin/env bash
+echo "Gradle requires Java 11"
+./gradlew sonarqube  -D"sonar.projectKey=SpaceUp-Server" -D"sonar.host.url=https://sonar.iatlas.dev" -D"sonar.login=***REMOVED***"
