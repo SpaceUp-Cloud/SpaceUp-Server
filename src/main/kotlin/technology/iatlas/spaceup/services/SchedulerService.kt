73e001fd5d216fc4ab30921e4ebadeeb1c8a1f82
Thraax Session <thraax.session@iatlas.technology>
2021-12-18 16:34:05 +0100

    Updated update frequency + updated scheduler as services are not necessary
    Signed-off-by: Thraax Session <thraax.session@iatlas.technology>

--- src/main/kotlin/technology/iatlas/spaceup/services/SchedulerService.kt
+++ src/main/kotlin/technology/iatlas/spaceup/services/SchedulerService.kt
@@ -8,24 +8,15 @@ import org.slf4j.LoggerFactory
 @Context
 class SchedulerService(
     private val domainService: DomainService,
-    private val serviceService: ServiceService
     ) {
     private val log = LoggerFactory.getLogger(SchedulerService::class.java)
 
-    @Scheduled(fixedDelay = "\${spaceup.scheduler.domains.update}")
+    @Scheduled(fixedRate = "\${spaceup.scheduler.domains.update}", initialDelay = "\${spaceup.scheduler.delayed}")
     internal fun updateDomainList() {
         log.debug("Update domain list")
         runBlocking {
             domainService.updateDomainList()
             domainService.list()
-        }
-    }
-
-    @Scheduled(fixedDelay = "\${spaceup.scheduler.services.update}", initialDelay = "\${spaceup.scheduler.delayed}")
-    internal fun updateServices() {
-        log.debug("Update service list")
-        runBlocking {
-            serviceService.list()
         }
     }
 
