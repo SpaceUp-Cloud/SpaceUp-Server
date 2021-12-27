9c5b6e35667a7ad0f9583a95727605a2a7060605
Thraax Session <thraax.session@iatlas.technology>
2021-12-27 09:51:33 +0100

    SU-31 WIP prepare database usage

--- src/main/kotlin/technology/iatlas/spaceup/core/startup/StartupEventListener.kt
+++ src/main/kotlin/technology/iatlas/spaceup/core/startup/StartupEventListener.kt
@@ -5,14 +5,18 @@ import io.micronaut.context.event.StartupEvent
 import io.micronaut.runtime.event.annotation.EventListener
 import jakarta.inject.Singleton
 import kotlinx.coroutines.runBlocking
+import org.dizitart.no2.migration.Instructions
+import org.dizitart.no2.migration.Migration
 import org.slf4j.LoggerFactory
 import technology.iatlas.spaceup.dto.Command
+import technology.iatlas.spaceup.services.DbService
 import technology.iatlas.spaceup.services.SshService
 
 @Singleton
 class StartupEventListener(
     env: Environment,
-    private val sshService: SshService
+    private val sshService: SshService,
+    private val dbService: DbService
 ) {
     private val log = LoggerFactory.getLogger(StartupEventListener::class.java)
 
@@ -47,4 +51,32 @@ class StartupEventListener(
             sshService.execute(Command(createDirectoryX))
         }
     }
+
+    private fun createDbAndInit() {
+        val db = dbService.createOrOpen()
+
+        val migration1 = Migration1(0, 1)
+        db.schemaVersion(0).addMigrations(migration1)
+            .openOrCreate("SpaceUp", "Spac3Up!***REMOVED***")
+
+    }
+}
+
+class Migration1(startVersion: Int, endVersion: Int) : Migration(startVersion, endVersion) {
+    override fun migrate(instructions: Instructions?) {
+        instructions
+            ?.forRepository("user")
+            ?.addField<String>("username")
+            ?.addField<String>("password")
+
+        instructions
+            ?.forRepository("ssh")
+            ?.addField<String>("username")
+            ?.addField<String>("password")
+
+        instructions
+            ?.forRepository("auth")
+            ?.addField<String>("jwtTime", "60")
+    }
+
 }
