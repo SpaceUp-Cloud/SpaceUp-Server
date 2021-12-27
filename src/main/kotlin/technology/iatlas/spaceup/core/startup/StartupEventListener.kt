d1dc7b3ddfb0c2f8ecd5bc2889c5f49ec09a30b8
TS <ts@iatlas.technology>
2021-12-27 19:42:36 +0100

    SU-17 Create directories locally

--- src/main/kotlin/technology/iatlas/spaceup/core/startup/StartupEventListener.kt
+++ src/main/kotlin/technology/iatlas/spaceup/core/startup/StartupEventListener.kt
@@ -1,21 +1,19 @@
 package technology.iatlas.spaceup.core.startup
 
+import com.lordcodes.turtle.ShellLocation
+import com.lordcodes.turtle.shellRun
 import io.micronaut.context.env.Environment
 import io.micronaut.context.event.StartupEvent
 import io.micronaut.runtime.event.annotation.EventListener
 import jakarta.inject.Singleton
-import kotlinx.coroutines.runBlocking
 import org.dizitart.no2.migration.Instructions
 import org.dizitart.no2.migration.Migration
 import org.slf4j.LoggerFactory
-import technology.iatlas.spaceup.dto.Command
 import technology.iatlas.spaceup.services.DbService
-import technology.iatlas.spaceup.services.SshService
 
 @Singleton
 class StartupEventListener(
     env: Environment,
-    private val sshService: SshService,
     private val dbService: DbService
 ) {
     private val log = LoggerFactory.getLogger(StartupEventListener::class.java)
@@ -27,6 +25,13 @@ class StartupEventListener(
     internal fun onApplicationEvent(event: StartupEvent) {
         log.info("Running SpaceUp startup")
 
+        val os = System.getProperty("os.name")
+        log.debug("OS: $os")
+        if(!os.lowercase().contains("linux")) {
+            log.warn("Currently only GNU/Linux is supported for local setup!")
+            return
+        }
+
         // Step 1: create directories if not exist
         createDirectories()
 
@@ -35,20 +40,16 @@ class StartupEventListener(
 
     private fun createDirectories() {
         log.info("Create remote directories")
-        val createDirectoryX = mutableListOf("mkdir", "-p", "")
 
-        val remoteHome = "~/.spaceup"
-        log.info("Create $remoteHome")
-        runBlocking {
-            createDirectoryX[2] = remoteHome
-            sshService.execute(Command(createDirectoryX))
-        }
+        val home = ShellLocation.HOME
+        val remoteHome = ".spaceup"
+        val remoteScriptDir = ".spaceup/tmp"
 
-        val remoteScriptDir = "~/.spaceup/tmp"
-        log.info("Create $remoteScriptDir")
-        runBlocking {
-            createDirectoryX[2] = remoteScriptDir
-            sshService.execute(Command(createDirectoryX))
+        shellRun(home) {
+            log.info("Create $remoteHome")
+            command("mkdir", listOf("-p", remoteHome))
+            log.info("Create $remoteScriptDir")
+            command("mkdir", listOf("-p", remoteScriptDir))
         }
     }
 
