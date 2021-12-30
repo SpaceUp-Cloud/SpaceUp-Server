6c08cf33d6af72464b68af731b1ee69750cbbe02
Thraax Session <thraax.session@gino-atlas.de>
2021-12-30 10:39:42 +0100

    SU-17 overworked installation process
    Signed-off-by: Thraax Session <thraax.session@gino-atlas.de>

--- src/main/kotlin/technology/iatlas/spaceup/core/interceptor/InstalledInterceptor.kt
+++ src/main/kotlin/technology/iatlas/spaceup/core/interceptor/InstalledInterceptor.kt
@@ -4,25 +4,24 @@ import io.micronaut.aop.InterceptorBean
 import io.micronaut.aop.MethodInterceptor
 import io.micronaut.aop.MethodInvocationContext
 import jakarta.inject.Singleton
-import org.dizitart.no2.filters.Filter
 import technology.iatlas.spaceup.core.annotations.Installed
 import technology.iatlas.spaceup.core.exceptions.NotInstalledException
+import technology.iatlas.spaceup.dto.Server
 import technology.iatlas.spaceup.services.DbService
 
 @Singleton
 @InterceptorBean(Installed::class)
-class InstallInterceptor(
+class InstalledInterceptor(
     private val dbService: DbService
 ) : MethodInterceptor<Any, Any> {
 
     override fun intercept(context: MethodInvocationContext<Any, Any>?): Any? {
         val db = dbService.getDb()
-        val serverCollection = db.getCollection("server")
+        val serverRepo = db.getRepository(Server::class.java)
 
-        val doc = serverCollection.find(Filter.ALL)
-        val installed = doc.first().get("installed")
-        if(installed == false) {
-            throw NotInstalledException("App is not properly installed! Run installer first.")
+        val server = serverRepo.find().first()
+        if(!server.installed) {
+            throw NotInstalledException("Application needs to be proper installed!")
         }
 
         return context?.proceed()
