08bbd1b0283efcb01b5fbae4af7a499235e2f1c6
Thraax Session <thraax.session@iatlas.technology>
2022-01-02 13:08:15 +0100

    SU-17 improved/fixed/make it run and finalize feature
    Signed-off-by: Thraax Session <thraax.session@iatlas.technology>

--- src/main/kotlin/technology/iatlas/spaceup/controller/api/InstallerController.kt
+++ src/main/kotlin/technology/iatlas/spaceup/controller/api/InstallerController.kt
@@ -2,15 +2,17 @@ package technology.iatlas.spaceup.controller.api
 
 import io.micronaut.http.HttpResponse
 import io.micronaut.http.HttpStatus
+import io.micronaut.http.MediaType
 import io.micronaut.http.annotation.Body
 import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Header
 import io.micronaut.http.annotation.Post
 import org.slf4j.LoggerFactory
 import technology.iatlas.spaceup.core.annotations.Installation
-import technology.iatlas.spaceup.dto.Server
 import technology.iatlas.spaceup.dto.Ssh
 import technology.iatlas.spaceup.dto.User
 import technology.iatlas.spaceup.services.DbService
+import technology.iatlas.spaceup.services.InstallerService
 
 /**
  * This is the installer endpoint to create a proper application setup.
@@ -19,86 +21,51 @@ import technology.iatlas.spaceup.services.DbService
 @Installation
 @Controller("/api/installer")
 class InstallerController(
-    private val dbService: DbService
+    private val dbService: DbService,
+    private val installerService: InstallerService
 ) {
     private val log = LoggerFactory.getLogger(InstallerController::class.java)
-    private val steps = mutableMapOf<String, Boolean>()
-
-    init {
-        steps["createUser"] = false
-        steps["createSshSetup"] = false
-    }
-
+    private val steps = installerService.getAllSteps()
     /**
      * Create a new user for authentication on SpaceUp
      * @param User defines a user database object
      */
-    @Post(uri = "/step/createUser")
-    fun createUser(@Body user: User): HttpResponse<String> {
+    @Post(uri = "/step/createUser", processes = [MediaType.APPLICATION_JSON])
+    fun createUser(@Body user: User, @Header("X-SpaceUp-Key") apiKey: String): HttpResponse<String> {
         // TODO 29.12.2010: Create user service for reusage
-        val db = dbService.getDb()
-        val userRepo = db.getRepository(User::class.java)
-        val result = userRepo.insert(user)
 
-        if(result.affectedCount > userRepo.size()) {
-            log.info("User ${user.username} was created")
-            steps["createUser"] = true
-            return HttpResponse.ok("User successful created!")
+        if(installerService.getApiKey() != apiKey) {
+            return HttpResponse.status(HttpStatus.EXPECTATION_FAILED, "API Key is not valid!")
         }
 
-        steps["createUser"] = false
-        return HttpResponse.notAllowed()
+        return installerService.createUser(user)
     }
 
     /**
      * Create the ssh user for authentication via SSH
      * @param SshUser defines ssh user database object
      */
-    @Post(uri = "/step/createSshSetup")
-    fun createSshUser(@Body ssh: Ssh): HttpResponse<String> {
+    @Post(uri = "/step/createSshSetup", processes = [MediaType.APPLICATION_JSON])
+    fun createSshUser(@Body ssh: Ssh, @Header("X-SpaceUp-Key") apiKey: String): HttpResponse<String> {
 
-        val db = dbService.getDb()
-        val sshRepo = db.getRepository(Ssh::class.java)
-        val result = sshRepo.insert(ssh)
-
-        if(result.affectedCount > sshRepo.size()) {
-            steps["createSshSetup"] = true
-            return HttpResponse.ok("User successful created!")
+        if(installerService.getApiKey() != apiKey) {
+            return HttpResponse.status(HttpStatus.EXPECTATION_FAILED, "API Key is not valid!")
         }
 
-        steps["createSshSetup"] = false
-        return HttpResponse.notAllowed()
+        return installerService.createSshUser(ssh)
     }
 
     /**
      * This is the final step to finalize the installation. It needs to be run.
      */
     @Post(uri = "/step/final")
-    fun getFinalization(): MutableList<HttpResponse<String>> {
+    suspend fun getFinalization(@Header("X-SpaceUp-Key") apiKey: String): HttpResponse<String>{
         val responses = mutableListOf<HttpResponse<String>>()
-        steps.forEach { (t, u) ->
-            if(!u) {
-                responses.add(
-                    HttpResponse.status(
-                        HttpStatus.EXPECTATION_FAILED,
-                        "Step '$t' was not processed!"
-                    )
-                )
-            }
+
+        if(installerService.getApiKey() != apiKey) {
+            return HttpResponse.status(HttpStatus.EXPECTATION_FAILED, "API Key is invalid")
         }
 
-        // Seems to be fine everything
-        if(responses.size == 0) {
-            // Set installation to be done
-            val db = dbService.getDb()
-            val serverRepo = db.getRepository(Server::class.java)
-
-            val serverDto = Server(true)
-            serverRepo.update(serverDto)
-
-            responses.add(HttpResponse.ok("Installation done. Set system to state 'installed'"))
-        }
-
-        return responses
+        return installerService.finalizeInstallation()
     }
 }
