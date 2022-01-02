1212bfa5c156ad630818de8dbd4683cd5b37fe98
Thraax Session <thraax.session@iatlas.technology>
2022-01-02 21:38:01 +0100

    Updated Copyright
    Signed-off-by: Thraax Session <thraax.session@iatlas.technology>

--- src/main/kotlin/technology/iatlas/spaceup/services/DbService.kt
+++ src/main/kotlin/technology/iatlas/spaceup/services/DbService.kt
@@ -1,3 +1,14 @@
+/*
+ * Copyright (c) 2022 Gino Atlas.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+
 package technology.iatlas.spaceup.services
 
 import io.micronaut.context.annotation.Context
@@ -19,7 +30,7 @@ import java.io.File
 
 @Context
 class DbService(
-    private val spaceupPathConfig: SpaceupPathConfig
+    spaceupPathConfig: SpaceupPathConfig
 ) {
     private val log = LoggerFactory.getLogger(DbService::class.java)
     private lateinit var db: Nitrite
@@ -53,7 +64,6 @@ class DbService(
             .loadModule(storeModule)
             .schemaVersion(1)
             //.addMigrations(migration1)
-                //"SpaceUp", "Spac3Up!#"
             .openOrCreate()*/
 
         db = nitrite("SpaceUp", "Spac3Up!#") {
