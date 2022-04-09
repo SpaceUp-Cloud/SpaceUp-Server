f465a9ca771fd3f5fddb4c0a64f36fdf90994b84
Thraax Session <thraax.session@gino-atlas.de>
2022-04-09 10:43:07 +0200

    Pass SONAR_TOKEN as parameter
    Signed-off-by: Thraax Session <thraax.session@gino-atlas.de>

--- sonar-scan.sh
+++ sonar-scan.sh
@@ -10,4 +10,5 @@
 #
 
 echo "Gradle requires Java 11"
-./gradlew sonarqube  -D"sonar.projectKey=SpaceUp-Server" -D"sonar.host.url=https://sonar.iatlas.dev" -D"sonar.login=2cbf3a38f3f7e3a580a3a83e18bb574df8793ffc"
+SONAR_TOKE=$1
+./gradlew sonarqube  -D"sonar.projectKey=SpaceUp-Server" -D"sonar.host.url=https://sonar.iatlas.dev" -D"sonar.login=$SONAR_TOKE"
