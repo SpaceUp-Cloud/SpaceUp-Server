5b515b65c967ae2c5730bdc173dd958c859afa08
Thraax Session <thraax.session@iatlas.technology>
2022-12-24 15:37:22 +0100

    Fix validation feedback isOK
    Signed-off-by: Thraax Session <thraax.session@iatlas.technology>

--- src/main/kotlin/technology/iatlas/spaceup/Application.kt
+++ src/main/kotlin/technology/iatlas/spaceup/Application.kt
@@ -145,14 +145,14 @@ fun String.decrypt(secret: String): String {
 
 fun Feedback.toHttpResponse(): HttpResponse<Feedback> {
     return if(this.isOk()) {
-        HttpResponse.created(this)
+        HttpResponse.ok(this)
     } else {
         HttpResponse.badRequest(this)
     }
 }
 
 fun Feedback.isOk(): Boolean {
-    return if((this.info.isNotEmpty() && this.error.isEmpty()) || this.error.isEmpty()) {
+    return if(this.info.isNotEmpty() && this.error.isEmpty() || (this.info.isEmpty() && this.error.isEmpty())) {
         true
     } else if (this.error.isNotEmpty()) {
         false
